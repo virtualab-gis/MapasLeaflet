450b28dbd2022a6e70fe55f77fbec270fb3bae7c
Virtualab-GIS <50464955+virtualab-gis@users.noreply.github.com>
2019-09-27 16:32:26 -0500

    Add files via upload

---
+++ json/js/biblioteca_central.js
@@ -0,0 +1,8 @@
+var bc = {
+"type": "FeatureCollection",
+"name": "biblioteca_central",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "full_id": "w437171951", "osm_id": "437171951", "osm_type": "way", "name": "Biblioteca Central UNAM" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -99.1872097, 19.3335658 ], [ -99.1870773, 19.3335818 ], [ -99.1870353, 19.3332713 ], [ -99.1875859, 19.3332049 ], [ -99.1876206, 19.3334607 ], [ -99.1872023, 19.3335111 ], [ -99.1872097, 19.3335658 ] ] ] ] } }
+]
+};
